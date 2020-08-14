5b54ea2e31cda5baa4ae33e4b13c348c5fb9433e
David <59340666+dekennedy@users.noreply.github.com>
2020-08-13 21:25:15 -0400

    Create Hello World Test.py
    Test import and print

---
+++ Hello World Test.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World")
+
+
+# In[5]:
+
+
+import sys
+get_ipython().system('conda install --yes --prefix {sys.prefix} numpy')
+
+
+# In[1]:
+
+
+print("asdfasdf")
+
+
+# In[3]:
+
+
+import sys
+get_ipython().system('conda install --yes --prefix {sys.prefix} pandas')
+
+
+# In[4]:
+
+
+import pandas as pd
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
